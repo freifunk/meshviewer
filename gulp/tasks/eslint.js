76753bf17f2e23175cedc6282f5a507dd9ea6d9a
weeman <weeman@frankfurt.ccc.de>
2023-09-18 22:05:35 +0200

    Migrate to Vite
    Signed-off-by: weeman <weeman@frankfurt.ccc.de>

--- gulp/tasks/eslint.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (gulp, plugins, config, env) {
-  return function eslint() {
-    return gulp
-      .src(["app.js", "gulpfile.js", "lib/**/*.js", "gulp/**/*.js"])
-      .pipe(plugins.eslint())
-      .pipe(plugins.eslint.format())
-      .pipe(env.production(plugins.eslint.failAfterError()));
-  };
-};
