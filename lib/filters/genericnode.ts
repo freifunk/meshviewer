9dd2a6d28c0b030a03350a12d5026a0a0f7767c1
Igor Scheller <igor.scheller@igorshp.de>
2023-10-27 18:21:38 +0200

    Update genericnode and nodefilter to use ts

---
+++ lib/filters/genericnode.ts
@@ -0,0 +1,69 @@
+import * as helper from "../utils/helper";
+import { Filter } from "../datadistributor";
+import { CanRender } from "../container";
+import { Node } from "../utils/node";
+
+export const GenericNodeFilter = function (
+  name: string,
+  keys: string[],
+  value: string,
+  nodeValueModifier: (a: any) => string,
+): Filter & CanRender {
+  let negate = false;
+  let refresh: () => any;
+
+  let label = document.createElement("label");
+  let strong = document.createElement("strong");
+  label.textContent = name + ": ";
+  label.appendChild(strong);
+
+  function run(node: Node) {
+    let nodeValue = helper.dictGet(node, keys.slice(0));
+
+    if (nodeValueModifier) {
+      nodeValue = nodeValueModifier(nodeValue);
+    }
+
+    return nodeValue === value ? !negate : negate;
+  }
+
+  function setRefresh(f: () => any) {
+    refresh = f;
+  }
+
+  function draw(el: HTMLElement) {
+    if (negate) {
+      el.classList.add("not");
+    } else {
+      el.classList.remove("not");
+    }
+
+    strong.textContent = value;
+  }
+
+  function render(el: HTMLElement) {
+    el.appendChild(label);
+    draw(el);
+
+    label.onclick = function onclick() {
+      negate = !negate;
+
+      draw(el);
+
+      if (refresh) {
+        refresh();
+      }
+    };
+  }
+
+  function getKey() {
+    return value.concat(name);
+  }
+
+  return {
+    run: run,
+    setRefresh: setRefresh,
+    render: render,
+    getKey: getKey,
+  };
+};
