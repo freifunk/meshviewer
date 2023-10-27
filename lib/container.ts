6f3133b35be7fb6bcf585e914b5f7aa490e4c86f
Igor Scheller <igor.scheller@igorshp.de>
2023-10-27 16:09:21 +0200

    Update container ts

---
+++ lib/container.ts
@@ -0,0 +1,26 @@
+export interface CanRender {
+  render: (element: HTMLElement) => any;
+}
+
+export interface CanAdd {
+  add: (element: CanRender) => any;
+}
+
+export const Container = function (tag: string): CanRender & CanAdd {
+  if (!tag) {
+    tag = "div";
+  }
+
+  let self = this;
+  let container = document.createElement(tag);
+
+  self.add = function add(d: CanRender) {
+    d.render(container);
+  };
+
+  self.render = function render(el: HTMLElement) {
+    el.appendChild(container);
+  };
+
+  return self;
+};
