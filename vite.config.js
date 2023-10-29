cc53cdaf77c1809cb1384070001e6642fc2d0cdf
anoy. <anoymouserver+github@mailbox.org>
2023-10-29 23:23:17 +0100

    fix deployment to subfolders

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "./",
   resolve: {
     alias: {
       "@fonts": resolve(__dirname, "assets/fonts"),
